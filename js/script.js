f5fac4c3765ace246c4dd49ee06c6f7186f2af93
rc2pc2 <petriccarcc@gmail.com>
2023-11-09 12:57:05 +0100

    first snack example

--- js/script.js
+++ js/script.js
@@ -23,11 +23,25 @@
 // ? Chiediamo all'utentessa e o utente la sua eta' e comunichiamo l'anno di nascita
 
 
-const userAge = parseInt(prompt('Type your age'));
+// const userAge = parseInt(prompt('Type your age'));
 
-const currentYear = 2023;
+// const currentYear = 2023;
 
-const birthYear = currentYear - userAge;
-console.log(birthYear);
+// const birthYear = currentYear - userAge;
+// console.log(birthYear);
 
-document.getElementById('output').innerHTML = 'Age is ' + birthYear;
+// document.getElementById('output').innerHTML = 'Age is ' + birthYear;
+
+
+// ? Chiediamo all'utente 3 numeri, li sommo e li mostro a schermo.
+
+let sum = 0;
+
+const firstNumber = parseInt( prompt ('Type a number' ));
+const secondNumber = parseInt( prompt ('Type a number' ));
+const thirdNumber = parseInt( prompt ('Type a number' ));
+
+sum = firstNumber + secondNumber + thirdNumber;
+
+document.getElementById('output').innerHTML = 'La somma dei numeri inseriti e\': ' + sum;
+
